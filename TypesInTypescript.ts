2cd044ab89171c7ad1708528417baac5c06882ff
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-18 10:31:15 +0530

    Initial commit

---
+++ TypesInTypescript.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Types in TypeScript
+ * 
+ * Below are all primitive types in TS
+ * string -> store text
+ * number -> intergers, real
+ * boolean
+ * undefined
+ * null
+ * bigint
+ * symbol
+ */
