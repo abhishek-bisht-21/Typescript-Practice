357b289ac59ca67c8e8088fefe436e76310dd70f
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-18 10:40:12 +0530

    Types in Typescript

--- TypesInTypescript.ts
+++ TypesInTypescript.ts
@@ -11,3 +11,17 @@
  * bigint
  * symbol
  */
+
+// let <variable_name> : number = value;  Type annotation or type signature
+
+let id:number = 6;
+let firstname: string = "abhishek";
+// firstname = 12; // error TS understands that firstname will store string
+var lastname = "bisht"
+// lstnmame = false
+console.log(id, firstname);
+
+
+// Union of types
+let userId: number | string = "26"
+userId = 10;
