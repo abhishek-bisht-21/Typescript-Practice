7fe0305c1db91dcf9dde4f4d2e4b54bef31c4137
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-18 11:20:52 +0530

    Any variable in TS

---
+++ 5.any.ts
@@ -0,0 +1,11 @@
+/**
+ * any type object
+ * 
+ * if you want to define a variable with type any, then either mention ": any" while declaring the variable
+ * or donot assign a value while declaring a variable
+ */
+
+
+let x; // this variable x is of type any
+x = 10;
+x = "10"
